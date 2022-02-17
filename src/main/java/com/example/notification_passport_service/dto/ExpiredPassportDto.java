020a3851b033103d64a1d74689c7c3a3e95210b1
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-02-17 20:54:03 +0300

    some

--- src/main/java/com/example/notification_passport_service/dto/ExpiredPassportDto.java
+++ src/main/java/com/example/notification_passport_service/dto/ExpiredPassportDto.java
@@ -1,10 +1,12 @@
 package com.example.notification_passport_service.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Builder
 public class ExpiredPassportDto {
 
     private Long id;
