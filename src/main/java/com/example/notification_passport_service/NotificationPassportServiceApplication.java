cba4a4341f52eda55a7724dc0c7f20f493521728
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-02-17 09:06:25 +0300

    Initial commit

---
+++ src/main/java/com/example/notification_passport_service/NotificationPassportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notification_passport_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationPassportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationPassportServiceApplication.class, args);
+    }
+
+}
